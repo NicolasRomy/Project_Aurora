08a445e979be824a567ee5482d659d9c090e1c2e
Quentin <bm.quentin@icloud.com>
2021-04-06 17:59:38 +0200

    hiérarchie ajout config.php vide et structur index

---
+++ code/php/page/index.php
@@ -0,0 +1,14 @@
+<?php include_once '../action/config.php'; ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="../../css/master.css"/>
+    <link rel="stylesheet" href="/css/master.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
