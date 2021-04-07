522c949b695962ca20626b337012f1e85357abef
Quentin <bm.quentin@icloud.com>
2021-04-07 11:31:23 +0200

    session start dans config.php

--- code/php/action/config.php
+++ code/php/action/config.php
@@ -1,9 +1,8 @@
 <?php
+session_start();
 $pdo = new PDO(
-        'mysql:host=localhost;dbname=bd_aurora;',
-        'root',
-        '',
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-?>
+  'mysql:host=localhost;dbname=bd_aurora;',
+  'root',
+  '',
+  array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
