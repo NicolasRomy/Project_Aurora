7afdf3b20776c4bc90c4e098ab2c87739b190a66
unknown <ecros@gamingcampus.local>
2021-04-07 10:55:59 +0200

    connexion

--- code/php/action/connexion.php
+++ code/php/action/connexion.php
@@ -1,20 +1,21 @@
+
 <?php
+    //Session start import of config
     session_start();
     $_SESSION;
-    
+
     require_once "config.php";
 
-    $sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password=MD5('".$_POST['password']."')";
+
+    $sql = "SELECT * FROM admin WHERE PSEUDO='".$_POST['username']."'";
     $pre = $pdo->prepare($sql);
     $pre->execute();
     $user = current($pre->fetchAll(PDO::FETCH_ASSOC));
-    if(empty($user)) {
-        header('Location:../connexion.php');
-    } else {
+    //loop to check password and act acordingly
+    if(password_verify($_POST['password'], $user['PASSWORD'])) {
         $_SESSION['user'] = $user;
-    }
-
-    if(isset($_SESSION['user'])) {
-        header('Location:../Page_dAcceuil.php');
+        header('Location:../accueil.php');
+    } else {
+        header('Location:../connexion.php');
     }
 ?>
