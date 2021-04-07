da98acca7f7a7e3e7944282465e5a323ff6fa82f
NicolasRomy <nromy@gaming.tech>
2021-04-07 11:29:40 +0200

    navbar

--- code/php/page/index.php
+++ code/php/page/index.php
@@ -13,13 +13,13 @@
     <header>
     <nav>
     <div class="nav-wrapper">
-      <form>
-        <div class="input-field">
-          <input id="search" type="search" required>
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-          <i class="material-icons">close</i>
-        </div>
-      </form>
+      <a href="#!" class="brand-logo"><img src="../../../assets/logo.png"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html"><i class="material-icons">search</i></a></li>
+        <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+        <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+      </ul>
     </div>
   </nav>
     </header>
