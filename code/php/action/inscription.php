9475079ee92fa898f48fbda630e51b74aaa9b576
Quentin <bm.quentin@icloud.com>
2021-04-08 10:08:26 +0200

    password hash

--- code/php/action/inscription.php
+++ code/php/action/inscription.php
@@ -18,12 +18,12 @@
         header('Location: ../page/login.php');
     }
     else {
-        
-        $sql = "INSERT INTO users(pseudo, email, password) VALUES(:pseudo,:email, password_hash(:password))";
+
+        $sql = "INSERT INTO users(pseudo, email, password) VALUES(:pseudo,:email, :password)";
         $dataBinded=array(
             ':pseudo'   => $_POST['pseudo'],
             ':email'   => $_POST['email'],
-            ':password'=> $_POST['password']
+            ':password'=> password_hash($_POST['password'], PASSWORD_DEFAULT),
         );
         $pre = $pdo->prepare($sql);
         $pre->execute($dataBinded);
