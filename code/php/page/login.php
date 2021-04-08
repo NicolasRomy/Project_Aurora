da0003d5680cf0ba6146ec6329e0c4c0f9922bc8
unknown <ecros@gamingcampus.local>
2021-04-08 10:16:14 +0200

    inscription

--- code/php/page/login.php
+++ code/php/page/login.php
@@ -4,12 +4,14 @@
 <html lang="fr" dir="ltr">
 <head>
   <link rel="stylesheet" href="../../css/master.css"/>
-  <?php include '../content/head.php'; ?>
+  <link type="text/css" rel="stylesheet" href="../../css/materialize.css"  media="screen,projection"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title></title>
 </head>
 <body>
   <header>
-    <?php include '../content/navbar.php'; ?>
+    <!-- menu -->
     <br><br>
   </header>
 
@@ -36,33 +38,10 @@
     } ?>
 
 
-    <form action="../action/inscription.php" method="post" class="col s12">
-      <div class="row">
-        <div class="input-field col s4">
-          <i class="material-icons prefix">face</i>
-          <input id="pseudo" type="text" class="validate">
-          <label for="pseudo">pseudo</label>
-        </div>
-        <div class="input-field col s4">
-          <i class="material-icons prefix">email</i>
-          <input id="email" type="text" class="validate">
-          <label for="email">Email</label>
-        </div>
-        <div class="input-field col s4">
-          <i class="material-icons prefix">lock_outline</i>
-          <input id="password" type="password" class="validate">
-          <label for="password">Mot De Passe</label>
-        </div>
-        <div class="col s6">
-          <input class="btn" type="submit" name="" value="inscription">
-        </div>
-      </div>
-    </form>
 
   </div>
 
   <footer>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   </footer>
 
   <script type="text/javascript" src="../../js/materialize.js">
