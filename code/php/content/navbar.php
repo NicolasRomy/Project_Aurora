ea03e029ca7ce7bd7fef23540b5874c695a811b5
Quentin <bm.quentin@icloud.com>
2021-04-08 14:48:59 +0200

    navbar

--- code/php/content/navbar.php
+++ code/php/content/navbar.php
@@ -1,16 +1,12 @@
-<div class="row">
-  <div class="col offset-m1 offset-m1 offset-l1 offset-xl1 s10 m10 l10 xl10 navbar-fixed position-navbar">
-    <nav class="raduis-navbar">
-      <div class="nav-wrapper">
-        <a href="#!" class="brand-logo"><img class="responsive-img img-navbar-logo" src="../../../assets/logo-big.PNG" alt=""></a>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="collapsible.html"><i class="material-icons">shopping_cart</i>Panier</a></li>
-          <li><a href="mobile.html"></a><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></li>
-        </ul>
-      </div>
-    </nav>
-
-  </div>
-
+<div class="margin-nav navbar-fixed position-navbar">
+  <nav class="raduis-navbar">
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo margin-left height-logo-a"><img class="responsive-img img-navbar-logo" src="../../../assets/logo-big.PNG" alt=""></a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a class="height-panier-a hover" href=""><img class="responsive-img img-navbar-panier" src="../../../assets/panier.PNG" alt=""><img class="responsive-img img-navbar-mon-panier" src="../../../assets/mon-panier.PNG" alt=""></a></li>
+        <li><a class="height-user-a hover" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
+      </ul>
+    </div>
+  </nav>
 </div>
