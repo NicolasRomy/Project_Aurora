dd8f117ef34d502ddf41985666983eece7732f52
Quentin <bm.quentin@icloud.com>
2021-04-08 11:13:32 +0200

    navbar

--- code/php/content/navbar.php
+++ code/php/content/navbar.php
@@ -1,12 +1,10 @@
 <div class="row">
   <div class="col offset-m1 offset-m1 offset-l1 offset-xl1 s10 m10 l10 xl10 navbar-fixed position-navbar">
-    <nav>
+    <nav class="navbar-raduis">
       <div class="nav-wrapper">
         <a href="#!" class="brand-logo"><img src="../../../assets/logo.png" alt=""></a>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">Javascript</a></li>
           <li><a href="mobile.html">Mobile</a></li>
         </ul>
