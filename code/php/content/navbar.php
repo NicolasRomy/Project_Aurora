f0ba26cacc70b33e574955e1812815c919998edd
Quentin <bm.quentin@icloud.com>
2021-04-08 14:52:47 +0200

    nav presque ok manque search

--- code/php/content/navbar.php
+++ code/php/content/navbar.php
@@ -5,7 +5,7 @@
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a class="height-panier-a hover" href=""><img class="responsive-img img-navbar-panier" src="../../../assets/panier.PNG" alt=""><img class="responsive-img img-navbar-mon-panier" src="../../../assets/mon-panier.PNG" alt=""></a></li>
-        <li><a class="height-user-a hover" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
+        <li><a class="height-user-a hover margin" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
       </ul>
     </div>
   </nav>
