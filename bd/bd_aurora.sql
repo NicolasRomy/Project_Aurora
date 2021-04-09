b7c3e66b2ff1d34c44f696ef12106b9910eb0f34
NicolasRomy <nromy@gaming.tech>
2021-04-09 17:26:17 +0200

    catalogue5

--- bd/bd_aurora.sql
+++ bd/bd_aurora.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 07 avr. 2021 à 08:51
+-- Généré le : ven. 09 avr. 2021 à 15:26
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -63,12 +63,23 @@ CREATE TABLE IF NOT EXISTS `comments` (
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jeux_idi` int(11) NOT NULL,
+  `jeux_id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
   `url` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `jeux_idi` (`jeux_idi`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `jeux_idi` (`jeux_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `images`
+--
+
+INSERT INTO `images` (`id`, `jeux_id`, `title`, `url`) VALUES
+(1, 2, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
+(2, 1, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
+(3, 3, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
+(4, 4, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
+(6, 5, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg');
 
 -- --------------------------------------------------------
 
@@ -86,7 +97,18 @@ CREATE TABLE IF NOT EXISTS `jeux` (
   `avis` text NOT NULL,
   `temps_jeux` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `jeux`
+--
+
+INSERT INTO `jeux` (`id`, `title`, `prix`, `synopsis`, `PEGI`, `avis`, `temps_jeux`) VALUES
+(1, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 18, 'blznlznlz', 60),
+(2, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 3, 'blznlznlz', 60),
+(3, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 12, 'blznlznlz', 60),
+(4, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 16, 'blznlznlz', 60),
+(5, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 7, 'blznlznlz', 60);
 
 -- --------------------------------------------------------
 
@@ -140,7 +162,7 @@ ALTER TABLE `comments`
 -- Contraintes pour la table `images`
 --
 ALTER TABLE `images`
-  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`jeux_idi`) REFERENCES `jeux` (`id`);
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`jeux_id`) REFERENCES `jeux` (`id`);
 
 --
 -- Contraintes pour la table `jeux_commandes`
