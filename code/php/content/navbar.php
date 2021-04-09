5a64b8204866927f2841d102c4577e5d7c4d27f4
Quentin <bm.quentin@icloud.com>
2021-04-09 11:24:16 +0200

    nav

--- code/php/content/navbar.php
+++ code/php/content/navbar.php
@@ -3,9 +3,14 @@
     <div class="nav-wrapper">
       <a href="#!" class="brand-logo margin-left height-logo-a"><img class="responsive-img img-navbar-logo" src="../../../assets/logo-big.PNG" alt=""></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a class="height-panier-a hover" href=""><img class="responsive-img img-navbar-panier" src="../../../assets/panier.PNG" alt=""><img class="responsive-img img-navbar-mon-panier" src="../../../assets/mon-panier.PNG" alt=""></a></li>
-        <li><a class="height-user-a hover margin" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
+      <ul class="hide-on-med-and-down">
+        <li class="input-field marge-search">
+          <input id="search" type="search" required>
+          <label class="label-icon icon-searc" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons icon-search">close</i>
+        </li>
+        <li class="right"><a class="height-panier-a hover" href=""><img class="responsive-img img-navbar-panier" src="../../../assets/panier.PNG" alt=""><img class="responsive-img img-navbar-mon-panier" src="../../../assets/mon-panier.PNG" alt=""></a></li>
+        <li class="right"><a class="height-user-a hover margin" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
       </ul>
     </div>
   </nav>
