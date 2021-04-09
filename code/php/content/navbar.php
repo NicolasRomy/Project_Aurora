83844d3cd52fb62ec70f82911d254d11065c850e
Quentin <bm.quentin@icloud.com>
2021-04-09 11:29:20 +0200

    href nav

--- code/php/content/navbar.php
+++ code/php/content/navbar.php
@@ -1,7 +1,7 @@
 <div class="margin-nav navbar-fixed position-navbar">
   <nav class="raduis-navbar">
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo margin-left height-logo-a"><img class="responsive-img img-navbar-logo" src="../../../assets/logo-big.PNG" alt=""></a>
+      <a href="index.php" class="brand-logo margin-left height-logo-a"><img class="responsive-img img-navbar-logo" src="../../../assets/logo-big.PNG" alt=""></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="hide-on-med-and-down">
         <li class="input-field marge-search">
@@ -10,7 +10,13 @@
           <i class="material-icons icon-search">close</i>
         </li>
         <li class="right"><a class="height-panier-a hover" href=""><img class="responsive-img img-navbar-panier" src="../../../assets/panier.PNG" alt=""><img class="responsive-img img-navbar-mon-panier" src="../../../assets/mon-panier.PNG" alt=""></a></li>
-        <li class="right"><a class="height-user-a hover margin" href=""><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
+        <?php if (isset($_SESSION['user'])){
+          $hrefUser = 'user.php';
+        }
+        else{
+          $hrefUser = 'login.php';
+        } ?>
+        <li class="right"><a class="height-user-a hover margin" href="<?php echo $hrefUser ?>"><img class="responsive-img img-navbar-user" src="../../../assets/user-logo.PNG" alt=""></a></li>
       </ul>
     </div>
   </nav>
