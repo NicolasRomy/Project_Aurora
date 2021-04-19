96e3a426e93369fadda283cc04b84162667d4c48
Quentin <bm.quentin@icloud.com>
2021-04-19 09:40:07 +0200

    maj bd

--- bd/bd_aurora.sql
+++ bd/bd_aurora.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 09 avr. 2021 à 15:26
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Généré le : lun. 19 avr. 2021 à 07:39
+-- Version du serveur :  8.0.23
+-- Version de PHP : 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `commandes`;
 CREATE TABLE IF NOT EXISTS `commandes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `prix_total` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `prix_total` int NOT NULL,
   `adresse` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
@@ -45,10 +45,10 @@ CREATE TABLE IF NOT EXISTS `commandes` (
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jeux_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `jeux_id` int NOT NULL,
   `content` text NOT NULL,
-  `note` int(11) NOT NULL,
+  `note` int NOT NULL,
   `pseudo` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `jeux_id` (`jeux_id`)
@@ -62,24 +62,13 @@ CREATE TABLE IF NOT EXISTS `comments` (
 
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jeux_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `jeux_idi` int NOT NULL,
   `title` varchar(50) NOT NULL,
   `url` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `jeux_idi` (`jeux_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `images`
---
-
-INSERT INTO `images` (`id`, `jeux_id`, `title`, `url`) VALUES
-(1, 2, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
-(2, 1, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
-(3, 3, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
-(4, 4, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg'),
-(6, 5, 'DARK SOULS 3 COVER', 'https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg');
+  KEY `jeux_idi` (`jeux_idi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,26 +78,16 @@ INSERT INTO `images` (`id`, `jeux_id`, `title`, `url`) VALUES
 
 DROP TABLE IF EXISTS `jeux`;
 CREATE TABLE IF NOT EXISTS `jeux` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `prix` float NOT NULL,
   `synopsis` text NOT NULL,
-  `PEGI` int(11) NOT NULL,
+  `PEGI` int NOT NULL,
   `avis` text NOT NULL,
-  `temps_jeux` int(11) NOT NULL,
+  `temps_jeux` int NOT NULL,
+  `coeur` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `jeux`
---
-
-INSERT INTO `jeux` (`id`, `title`, `prix`, `synopsis`, `PEGI`, `avis`, `temps_jeux`) VALUES
-(1, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 18, 'blznlznlz', 60),
-(2, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 3, 'blznlznlz', 60),
-(3, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 12, 'blznlznlz', 60),
-(4, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 16, 'blznlznlz', 60),
-(5, 'DARK SOULS 3', 65, 'yadi yadi yada dark souls 3', 7, 'blznlznlz', 60);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -118,9 +97,9 @@ INSERT INTO `jeux` (`id`, `title`, `prix`, `synopsis`, `PEGI`, `avis`, `temps_je
 
 DROP TABLE IF EXISTS `jeux_commandes`;
 CREATE TABLE IF NOT EXISTS `jeux_commandes` (
-  `id_commande` int(11) NOT NULL,
-  `id_jeux` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
+  `id_commande` int NOT NULL,
+  `id_jeux` int NOT NULL,
+  `quantity` int NOT NULL,
   KEY `id_jeux` (`id_jeux`),
   KEY `id_commande` (`id_commande`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -133,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `jeux_commandes` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `admin` tinyint(1) NOT NULL,
   `pseudo` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -162,7 +141,7 @@ ALTER TABLE `comments`
 -- Contraintes pour la table `images`
 --
 ALTER TABLE `images`
-  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`jeux_id`) REFERENCES `jeux` (`id`);
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`jeux_idi`) REFERENCES `jeux` (`id`);
 
 --
 -- Contraintes pour la table `jeux_commandes`
