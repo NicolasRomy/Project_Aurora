29bc929dee4b08109165ac80bb58c7a10c58f8c0
Quentin <bm.quentin@icloud.com>
2021-04-19 09:55:02 +0200

    structur accueil

--- code/php/page/index.php
+++ code/php/page/index.php
@@ -12,7 +12,17 @@
       <?php include '../content/navbar.php'; ?>
     </header>
 
+    <h2>Bienvenue sur Aurora, le site d'achat de jeu vidéo conçu pour les parents.</h2>
+    <p>Acheter des jeux pour vos enfants en toute confiance grâce à nos différents indicateurs</p>
+
     <img src="../../../assets\background_gradiant.svg" alt="background">
+
+    <h2>Nos jeux par PEGI</h2>
+    <p>Le PEGI est un système d'évaluation des jeux vidéo, créé pour aider les consommateurs
+      (en particulier, les parents) à s'informer de chaque type de jeux vidéo à l'aide de
+      différents logos exposé sur leur boite d'origine.</p>
+
+      <!-- card age fixe 3 7 12 16 18 -->
 
     <img src="../../../assets\background_gradiant.svg" alt="background" class="reverse">
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
