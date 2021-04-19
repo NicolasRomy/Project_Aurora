70614128e89548aefa1b51c4adecf02e2706166e
Quentin <bm.quentin@icloud.com>
2021-04-19 15:35:11 +0200

    fichier temp

--- code/php/page/temp.php
+++ code/php/page/temp.php
@@ -1,3 +1,11 @@
 <?php if($_SESSION['user']['admin'] == 0){
   header('Location: ../page/index.php');
 } ?>
+
+
+
+
+Hitman 3 est un jeu d'infiltration dans lequel vous incarnez l'agent 47.
+Dans ce troisième épisode de la nouvelle trilogie lancée en 2017,
+six lieux sont disponible au lancement, mais il est possible de
+transférer les ancienne missions des deux premier volets.
