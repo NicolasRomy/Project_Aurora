8ec4d3653aa99477c857afd5d492d67ebb232be4
unknown <ecros@gamingcampus.local>
2021-04-20 12:53:15 +0200

    fin mardi

---
+++ code/php/content/pegi_explanation.php
@@ -0,0 +1,101 @@
+<link rel="stylesheet" href="../../css/pegi.css"/>
+
+<div class="row background ">
+    <div class="col m12 ok">
+        <div class="col m6 ">
+            <div class="col m12">
+                <div class="col offset-m1 m10 bd background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/violence.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu contient des scènes de violence. Dans les jeux classés PEGI 7, les scènes de violence ne peuvent être ni réalistes ni détaillées. Les jeux PEGI 12 peuvent contenir de la violence dans un environnement imaginaire ou une violence non réaliste par rapport à des personnages à figure humaine, alors que les jeux classés PEGI 16 ou 18 contiennent des scènes de violence de plus en plus réalistes.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/fear.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Ce descripteur peut apparaître sur des jeux PEGI 7 s’ils contiennent des images ou des sons susceptibles d’effrayer ou de faire peur aux jeunes enfants, ou sur des jeux PEGI 12 s’ils contiennent des sons ou des effets horrifiants (mais sans aucun contenu violent).</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/sexual-content.jpg" alt=""class="logo-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Ce descripteur peut accompagner une classification PEGI 12 si le jeu contient des positions ou des insinuations à caractère sexuel, une classification PEGI 16 s’il contient des scènes de nudité ou des rapports sexuels sans organes génitaux visibles ou une classification PEGI 18 s’il contient une activité sexuelle explicite.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok ko background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/discrimination.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu contient des représentations ethniques, religieuses, nationalistes, de genre ou autres stéréotypes susceptibles d’encourager la haine. Ce contenu est toujours limité à la classification PEGI 18 (et susceptible d’enfreindre la législation pénale nationale).</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col m6">
+            <div class="col m12">
+                <div class="col offset-m1 m10 bd background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/bad-language.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu contient un langage grossier. Ce descripteur peut apparaître sur les jeux classés PEGI 12 (grossièreté légère), PEGI 16 (jurons à caractère sexuel ou blasphèmes) ou PEGI 18 (jurons à caractère sexuel ou blasphèmes).</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/gambling.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu présente des contenus qui encouragent ou enseignent les jeux de hasard. Ces simulations de jeux concernent les jeux de hasard qui ont normalement lieu dans les casinos ou les salles de jeux de hasard. Les jeux ayant ce type de contenus sont classés PEGI 12, PEGI 16 ou PEGI 18.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/drugs.jpg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu se réfère à ou décrit la consommation de drogues illégales, d’alcool ou de tabac. Les jeux sur lesquels apparaissent ce descripteur de contenu sont toujours classés PEGI 16 ou PEGI 18.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col offset-m1 m10 bd ok ko background-text" >
+                    <div class="col m12">
+                        <div class="col m3">
+                            <img src="../../../assets/pegi/in-game-purchases.jpeg" alt=""class="-pegi bd"> 
+                        </div>
+                        <div class="col m9">
+                            <p>Le jeu contient des scènes de violence. Dans les jeux classés PEGI 7, les scènes de violence ne peuvent être ni réalistes ni détaillées. Les jeux PEGI 12 peuvent contenir de la violence dans un environnement imaginaire ou une violence non réaliste par rapport à des personnages à figure humaine, alors que les jeux classés PEGI 16 ou 18 contiennent des scènes de violence de plus en plus réalistes.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
