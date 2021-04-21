fd216ff823a674f1f008e553db99085d512fae22
unknown <ecros@gamingcampus.local>
2021-04-21 15:30:47 +0200

    ok

--- code/php/content/game_card.php
+++ code/php/content/game_card.php
@@ -18,7 +18,7 @@
             <div style= "background-size: cover; width: 90%; height: 180px; margin:0 auto; background-image:url(<?php echo $jeu['image']?>);border-radius: 15px;";>
             </div>
             <div style="text-align:center">
-            <h2 style="font-size: 15px; color : white ;margin:0 auto; font-weight:bold"> <?php echo $jeu['title']?></h2>
+            <h2 style="font-size: 15px; color : white ;margin:0 auto; font-weight:bold;white-space: nowrap; overflow: hidden;  text-overflow: ellipsis;"> <?php echo $jeu['title']?></h2>
             <p style="font-size: 12px; color : white; margin:0 auto;"> <?php echo $jeu['prix']?> €</p>
 
             </div>
