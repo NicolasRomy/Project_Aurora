e1bd3d989655b7bcfd37fe14400d4c870b9a920a
unknown <nromy@gamingcampus.local>
2021-04-22 16:54:44 +0200

    logo

--- code/php/content/game_card.php
+++ code/php/content/game_card.php
@@ -1,5 +1,10 @@
 
-
+    <?php
+    $sql= "SELECT * FROM jeux_plateforme jp INNER JOIN plateforme p ON p.id = jp.plateforme WHERE jeux=".$jeu['id'];
+    $pre = $pdo->prepare($sql);
+    $pre->execute();
+    $plateforms = $pre->fetchAll(PDO::FETCH_ASSOC); 
+    ?>
     <div class="card" style= "height: 260px ; width: 195px; border-radius: 15px;
      <?php  if ($jeu['PEGI'] == 18){
                 ?>background: rgb(255,97,97);
@@ -19,7 +24,11 @@
             }
                 ?>">
         <div class="card-image">
-        <img src="../../../assets/PlayStation-logo.png" style="height:30px; width:30px; margin-bottom: 10px">
+            <div style="display:flex;align-items:center;justify-content:center">
+                <?php foreach($plateforms as $plateform){ ?>
+                    <img src="../../../<?php echo $plateform['icon'] ?>" style="height:30px; width:30px; margin-bottom: 10px">
+                <?php } ?>
+            </div>
             <div style= "background-size: cover; width: 90%; height: 180px; margin:0 auto; background-image:url(<?php echo $jeu['image']?>);border-radius: 15px;";>
             </div>
             <div style="text-align:center">
