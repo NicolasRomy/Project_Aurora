dcfab9ffbfe63c3509c84e3e1977bf0c46a89eb2
Quentin <bm.quentin@icloud.com>
2021-04-22 17:06:28 +0200

    a

---
+++ code/php/page/adminMofiGame.php
@@ -0,0 +1,40 @@
+
+
+<?php
+include_once '../action/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <?php include '../content/head.php'; ?>
+  <title>ajout jeux</title>
+</head>
+
+<body>
+  <header>
+    <?php include '../content/navbar.php'; ?>
+  </header>
+
+  <div class="row">
+
+
+  </div>
+
+<?php include '../content/footer.php'; ?>
+
+
+    <script type="text/javascript" src="../../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../../js/materialize.js"></script>
+
+    <script>
+
+    $(document).ready(function(){
+
+      $('.sidenav').sidenav();
+      $('select').formSelect();
+    });
+
+    </script>
+  </body>
+  </html>
