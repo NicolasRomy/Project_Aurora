79590d914071d2c6264a6b94cebc0716c010db22
unknown <ecros@gamingcampus.local>
2021-04-24 17:28:25 +0200

    ok

--- code/php/content/head.php
+++ code/php/content/head.php
@@ -1,6 +1,9 @@
 <link type="text/css" rel="stylesheet" href="../../css/materialize.css"  media="screen,projection"/>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&display=swap" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="../../css/navbar.css"/>
 <link type="text/css" rel="stylesheet" href="../../css/margin.css"/>
+<link type="text/css" rel="stylesheet" href="../../css/font.css"/>
 <link rel="stylesheet" href="../../css/main.css"/>
 <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
