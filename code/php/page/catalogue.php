47b665534dffcf2d1504e7861d891c1317716fa4
unknown <nromy@gaming.tech>
2021-04-25 17:00:34 +0200

    lets go

--- code/php/page/catalogue.php
+++ code/php/page/catalogue.php
@@ -46,74 +46,73 @@
 <div id="myBtnContainer">
   <button class="waves-effect waves-light btn-small" onclick="filterSelection2('all')"> Show all</button>
   <button class="waves-effect waves-light btn-small" onclick="filterSelection2('0')"> coeur</button>
-  <button class="waves-effect waves-light btn-small" onclick="filterSelection2('1')"> pascoeur</button>
-
-
-
+  <button class="waves-effect waves-light btn-small" onclick="filterSelection2('1')"> pas coeur</button>
+</div>
 
 <?php
   //loop to display title one by one and create unique link to articles
   foreach($jeux as $jeu){
     ?>
       <div style="display:flex;">
-       <div class= 'filterDiv <?php echo $jeu['PEGI']?>' class = 'filterDiv2 <?php echo $jeu['coeur']?>'><?php gameCard($pdo, $jeu, 0);?></div><?php } ?>
+       <div class= 'filterDiv <?php echo $jeu['PEGI']?> 'filterDiv <?php echo $jeu['coeur']?> > <?php gameCard($pdo, $jeu, 0);?></div><?php } ?>
+      </div>
       </div>
       
 
 <script>
-filterrrr("all")
-function filterrrr(c){
-  x = document.getElementsByClassName("filterDiv");
-  $('.filterDiv').hide();
-  $('.'+c).show();}
 
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
-filterSelection2("all")
 function filterSelection2(c) {
   var x, i;
-  x = document.getElementsByClassName("filterDiv2");
+  x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
+// Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 
+// Hide elements that are not selected
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+// Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+  btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
